988000db70fdc87c9f066cd4579c83d07c31c411
rail-rain <rail91403@gmail.com>
2015-07-24 16:35:36 +0900

    first release

---
+++ README.md
@@ -0,0 +1,21 @@
+# html2hs-args
+
+pass the converted html into hyperscript to Second argument
+
+## demo
+
+``` js
+var html2hs = require('html2hs-args');
+var h = require('hyperscript');
+html2hs("<span><span></span></span>", h, function (err, data) {
+  console.log(data);// > <span><span></span></span>
+});
+```
+
+## cloned
+
+this is cloned [html2hscript](https://github.com/twilson63/html2hscript)
+And this was modified some of the source code of it
+
+Copyright (c) 2015 twilson63
+[MIT](http://opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,53 @@
+var Parser = require('htmlparser2').Parser;
+
+var elementStack = [];
+
+function ItemList(parent) {
+    this.parent = parent;
+    this.content = [];
+}
+
+ItemList.prototype.add = function (data) {
+    this.content.push(data);
+}
+
+module.exports = function(html, h, cb) {
+    var currentItemList = new ItemList(null);
+
+    var parser = new Parser({
+        onopentag: function (name, attribs) {
+            currentItemList = new ItemList(currentItemList);
+            elementStack.unshift([ name, attribs ]);
+        },
+        ontext: function (text) {
+            currentItemList.add(text);
+        },
+        onclosetag: function (tagname) {
+            var element = elementStack.shift();
+            var elementContent = currentItemList.content;
+
+            currentItemList = currentItemList.parent;
+
+            var attribs = element[1];
+
+            var id = attribs['id'];
+            var idSuffix = id !== undefined ? '#' + id : '';
+            delete attribs['id'];
+
+            var classNames = attribs['class'];
+            var classSuffix = (classNames !== undefined ? classNames : '').split(/\s+/g).filter(function (v) { return v.length > 0; }).map(function (cls) { return '.' + cls; }).join('');
+            delete attribs['class'];
+            
+            var item = h(element[0] + idSuffix + classSuffix,
+                attribs,
+                elementContent);
+            currentItemList.add(item);
+        },
+        onend: function () {
+          cb(null, currentItemList.content);
+        }
+    }, {decodeEntities: true});
+
+  parser.write(html);
+  parser.end();
+}
